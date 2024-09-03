787729491c8161ff3cff4e471011e81296823225
Akshat Agrawal <agrawalakshat582@gmail.com>
2024-09-03 16:24:14 +0530

    tp

--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/trendingHashtags", getTrendingHashtags);
 
 router.get("/timeline", getTimelinePosts);
+
 router.post('/', createPost);
 router.get('/:id', getPost);
 router.put('/:id', updatePost);
@@ -18,16 +19,6 @@ router.post("/:id/comment", addComment);
 router.get("/:id/comments", getComments);
 
 
-router.get("/hashtag/:hashtag", async (req, res) => {
-    const hashtag = req.params.hashtag;
-  
-    try {
-      const posts = await PostModel.find({ hashtags: hashtag });
-      res.status(200).json(posts);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
   
   
 
